1bbd0d8fd4781403c00835712e3393526791611d
sujin <sujinki6020@gmail.com>
2021-06-15 09:27:04 +0900

    Editing home APIs

--- src/main/java/com/project/knit/controller/HomeController.java
+++ src/main/java/com/project/knit/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.project.knit.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    // 가장 많이 조회된 문서 조회
-    // 최근 변경된 문서
+    // 가장 많이 조회된 문서 조회 (6)
+//    @GetMapping("/most/viewed")
+    // 최근 변경된 문서 (10)
+//    @GetMapping("/recent/changed")
+    // featured
+//    @GetMapping("/featured")
 
+    // search
+
+    // search by tag
 
 }
